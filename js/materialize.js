5daedb4faa348ea6ce4495af9939d5b949579cde
Adam Espino <aespino1987@gmail.com>
2019-12-05 19:04:04 -0600

    Initial Upload html and images

--- js/materialize.js
+++ js/materialize.js
@@ -14,6 +14,10 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 /*! cash-dom 1.3.5, https://github.com/kenwheeler/cash @license MIT */
+
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
 (function (factory) {
   window.cash = factory();
 })(function () {
